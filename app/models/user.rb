9dc4a3cc0aaceaee0cda8daa94a5bfeaccfcf58d
TheBonface <bonface_ochieng@outlook.com>
2016-06-03 13:14:11 +0300

    email and name validation test added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord::Base
-	validates :name, presence: true, length: {maimum:50}
-	validates :email, presence: true, length: {maximum:255}
+	validates :name, presence: true
+	validates :email, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,12 +6,13 @@ end
 test "should be valid" do
 assert @user.valid?
 end
-test "name should not be too long" do
-@user.name = "a" * 51
+
+test "name should be present" do
+@user.name = ""
 assert_not @user.valid?
 end
-test "email should not be too long" do
-@user.email = "a" * 244 + "@example.com"
+test "email should be present" do
+@user.email = ""
 assert_not @user.valid?
 end
 end
