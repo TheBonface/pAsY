52c65f1bc133a8051200d90514b0df978f20d8a7
TheBonface <bonface_ochieng@outlook.com>
2016-06-07 06:12:05 -0700

    password digest users generated

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -31,4 +31,10 @@ valid_addresses.each do |valid_address|
 @user.email = valid_address
 assert @user.valid?, "#{valid_address.inspect} should be valid"
 end
+test "email addresses should be unique" do
+duplicate_user = @user.dup
+duplicate_user.email = @user.email.upcase
+@user.save
+assert_not duplicate_user.valid?
+end
 end
