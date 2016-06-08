7346ffa0b452f964be54b30ee59d87fe0db3da8f
TheBonface <bonface_ochieng@outlook.com>
2016-06-08 15:58:05 -0700

    form added on the signup view and controller,helper , css added

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+	#Returns the Gravatar for thegiven user . 
+	# def gravatar_for(user)
+	# 	gravatar_id = Digest::MD5::hexadigest(user.email.downcase)
+	# 	gravatar_url="https://secure.gravatar.com/avatar/#{gravatar_id}"
+	# 	image_tag(gravatar_url, alt: user.name,class: "gravatar" )
+	# end
 end
