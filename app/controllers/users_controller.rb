cbb0a20c784cde3a4275aff28a6b9c416d0105f2
TheBonface <bonface_ochieng@outlook.com>
2016-07-13 16:11:34 +0300

    all changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
 before_action :logged_in_user , only: [:edit, :update]
 before_action :correct_user, only: [:edit, :update]
 
+def index 
+  @users =User.all 
+end 
+
   def show
     @user = User.find(params[:id])
   end
@@ -47,6 +51,7 @@ end
     #confirm a logge-in user 
     def logged_in_user 
       unless logged_in?
+        store_location 
         flash[:danger]= "Please log in."
         redirect_to login_url 
     end 
