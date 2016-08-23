d01968cffee7e2135df44766785c6009457e92f9
TheBonface <bonface_ochieng@outlook.com>
2016-08-23 17:52:11 +0300

    user reference to location added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808131342) do
+ActiveRecord::Schema.define(version: 20160822065942) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "address"
